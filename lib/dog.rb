07625f1f2c27eb77cda8e611b57d1e5a727769e1
Sonja Parsell <hello@sparsell.com>
2020-04-17 12:59:00 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
-# Add your code here
+class Dog
+  attr_reader :name
+
+  @@all = []
+
+  def initialize
+    @name = name
+    save
+  end
+
+  def save
+    @@all < self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all.each do |name|
+    puts name.name
+    end
+  end
+end
